c84066532f3d93ebc5d628c48029159966f57dae
angangi <angangi@outlook.com>
2021-01-10 21:38:26 +0800

    11

--- 11.盛最多水的容器.java
+++ 11.盛最多水的容器.java
@@ -7,7 +7,14 @@
 // @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-
+        int result = 0;
+        for (int i = 0; i < height.length-1; i++) {
+            for (int j = i+1; j < height.length; j++) {
+                int area = (j-i) * Math.min(height[i],height[j]);
+                result = Math.max(result, area);
+            }
+        }
+        return result;
     }
 }
 // @lc code=end
